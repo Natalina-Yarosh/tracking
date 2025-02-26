1f60792189cd35afdbb166b12c1531a53834cc8c
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-02-26 21:51:59 +0300

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
-  <h1 class="text-3xl">text</h1>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#" >
+      <img src="./assets/img/logo.png" alt="Logo" class="h-9">
+    </a>
+    <a href="#" class="text-sm">
+      <p v-if="false" class="flex items-center gap-1">
+        Day complete! 
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </p>
+      <div v-else class="flex items-center gap-1">
+       <p>Progress: <span class="font-mono">20%</span></p>
+       <div class="w-3 h-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+  <main>
+
+  </main>
 </template>
