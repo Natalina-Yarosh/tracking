c4b27205de0e958b8e1a15e63572add657ecba9e
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-06 17:40:29 +0300

    Разбиение кода на компоненты шапки и навигации

---
+++ src/components/TheHeaderProgress.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+<template>
+  <a href="#" class="text-sm">
+    <p v-if="false" class="flex items-center gap-1">
+      Day complete!
+      <CheckCircleIcon class="h-7 text-green-500" />
+    </p>
+    <div v-else class="flex items-center gap-1">
+      <p>Progress: <span class="font-mono">20%</span></p>
+      <div class="w-3 h-3 rounded-full bg-red-500"></div>
+    </div>
+  </a>
+</template>
