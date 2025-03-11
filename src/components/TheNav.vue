96e4a4dea44bd24b98763f93e1212af7ca8c006d
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-11 23:15:11 +0300

    Модуль с функциями

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,20 +1,10 @@
 <script setup>
 
-
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from './NavItem.vue'
-
-import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from '../constants'
+import { NAV_ITEMS } from '@/constants';
 
 defineProps(['currentPage'])
 const emit = defineEmits(['navigate'])
-
-const navItems = {
-  [PAGE_TIMELINE]: ClockIcon,
-  [PAGE_ACTIVITIES]: ListBulletIcon,
-  [PAGE_PROGRESS]: ChartBarIcon,
-}
-
 
 </script>
 
@@ -22,7 +12,7 @@ const navItems = {
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
       <NavItem
-        v-for="(icon, page) in navItems"
+        v-for="(icon, page) in NAV_ITEMS"
         :key="page"
         :href="`#${page}`"
         :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
