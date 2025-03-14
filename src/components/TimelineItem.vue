7a2e643a35241d664b6519a33e8dbdeda735c4a8
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-14 17:08:12 +0300

    Компонент активности

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -5,11 +5,11 @@ import { ref } from 'vue'
 import TimelineHour from './TimelineHour.vue'
 import { isTimeLineItemValid } from '@/validators'
 defineProps({
-    timelineItem:{
-        required:true,
-        type: Object,
-        validator: isTimeLineItemValid
-    }
+  timelineItem: {
+    required: true,
+    type: Object,
+    validator: isTimeLineItemValid,
+  },
 })
 
 const options = [
@@ -18,13 +18,17 @@ const options = [
   { label: 'Reading', value: 3 },
 ]
 
-const selectedActivityId = ref(1);
-
+const selectedActivityId = ref(1)
 </script>
 
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <TimelineHour :hour="timelineItem.hour"/>
-    <BaseSelect :selected="selectedActivityId" :options="options" placeholder="Rest" @select="selectedActivityId = $event"/>
+    <TimelineHour :hour="timelineItem.hour" />
+    <BaseSelect
+      :selected="selectedActivityId"
+      :options="options"
+      placeholder="Rest"
+      @select="selectedActivityId = $event"
+    />
   </li>
 </template>
