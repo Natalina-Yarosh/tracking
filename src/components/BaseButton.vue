a7eb0a011cc56a8317b79a387ca18043047f0717
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-14 23:17:42 +0300

    Скрипт инициализации модуля компонента

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,25 +1,25 @@
-<script setup>
-import { isButtonTypeValid } from '@/validators';
+<script>
 import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '../constants'
+
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 p-3 enabled:hover:bg-gray-200',
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 p-3 enabled:hover:bg-red-600 text-white',
+}
+</script>
+<script setup>
+import { isButtonTypeValid } from '@/validators'
 
 defineProps({
   type: {
     default: BUTTON_TYPE_NEUTRAL,
     type: String,
-    validate: isButtonTypeValid
-  }
+    validate: isButtonTypeValid,
+  },
 })
-
-  const typeClasses = {
-    [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 p-3 enabled:hover:bg-gray-200',
-    [BUTTON_TYPE_DANGER]: 'bg-red-500 p-3 enabled:hover:bg-red-600 text-white'
-  }
 </script>
 
 <template>
-  <button
-    :class="`${typeClasses[type]} rounded  disabled:cursor-not-allowed disabled:opacity-50`"
-  >
+  <button :class="`${typeClasses[type]} rounded  disabled:cursor-not-allowed disabled:opacity-50`">
     <slot></slot>
   </button>
 </template>
