cc93b9968d4e2613babb064109ca3ebb3d5ab3b0
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-03-15 13:30:31 +0300

    Форма создания активностей

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,9 +1,10 @@
 <script>
-import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '../constants'
+import {BUTTON_TYPE_PRIMARY,  BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '../constants'
 
 const typeClasses = {
   [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 p-3 enabled:hover:bg-gray-200',
   [BUTTON_TYPE_DANGER]: 'bg-red-500 p-3 enabled:hover:bg-red-600 text-white',
+  [BUTTON_TYPE_PRIMARY]: 'bg-blue-500 p-3 enabled:hover:bg-blue-600 text-white',
 }
 </script>
 <script setup>
@@ -11,7 +12,7 @@ import { isButtonTypeValid } from '@/validators'
 
 defineProps({
   type: {
-    default: BUTTON_TYPE_NEUTRAL,
+    default: BUTTON_TYPE_PRIMARY,
     type: String,
     validate: isButtonTypeValid,
   },
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,6 +1,9 @@
 <script setup>
+import BaseButton from '@/components/BaseButton.vue'
 import ActivityItem from '../components/ActivityItem.vue'
-import { validateActivities , isActivityValid} from '@/validators'
+import { validateActivities, isActivityValid } from '@/validators'
+import { PlusIcon } from '@heroicons/vue/24/outline'
+import { BUTTON_TYPE_PRIMARY } from '@/constants'
 
 defineProps({
   activities: {
@@ -11,7 +14,7 @@ defineProps({
 })
 
 const emit = defineEmits({
-  deleteActivity: isActivityValid
+  deleteActivity: isActivityValid,
 })
 </script>
 
@@ -25,5 +28,11 @@ const emit = defineEmits({
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
+    <form class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+      <input type="text" class="w-full rounder border px-4 text-xl" placeholder="Activity name"/>
+      <BaseButton :type="BUTTON_TYPE_PRIMARY">
+        <PlusIcon class="h-8"/>
+      </BaseButton>
+    </form>
   </div>
 </template>
