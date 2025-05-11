c9a5ccbf2ea8241bd8a55cefe8bf3aae701d88fe
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-05-11 21:05:04 +0300

    Рефакторинг структуры данных NAV_ITEMS

--- src/constants.js
+++ src/constants.js
@@ -4,11 +4,20 @@ export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 
-export const NAV_ITEMS = {
-  [PAGE_TIMELINE]: ClockIcon,
-  [PAGE_ACTIVITIES]: ListBulletIcon,
-  [PAGE_PROGRESS]: ChartBarIcon,
-}
+export const NAV_ITEMS = [
+  {
+    page: PAGE_TIMELINE,
+    icon: ClockIcon,
+  },
+  {
+    page: PAGE_ACTIVITIES,
+    icon: ListBulletIcon,
+  },
+  {
+    page: PAGE_PROGRESS,
+    icon: ChartBarIcon,
+  },
+]
 
 export const MINUTES_IN_HOUR = 60
 export const SECONDS_IN_MINUTE = 60
@@ -22,4 +31,10 @@ export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_PRIMARY = 'primary'
 export const BUTTON_TYPE_SUCCESS = 'success'
 export const BUTTON_TYPE_WARNING = 'warning'
-export const BUTTON_TYPES = [BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_PRIMARY, BUTTON_TYPE_SUCCESS, BUTTON_TYPE_WARNING]
+export const BUTTON_TYPES = [
+  BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_NEUTRAL,
+  BUTTON_TYPE_PRIMARY,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+]
