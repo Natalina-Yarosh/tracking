14af7e248701a3ead9e64afb117761d2865a88d7
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-05-12 14:35:33 +0300

    Синхронизация времени после удаления активности

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
 import BaseButton from '@/components/BaseButton.vue'
 import {
@@ -25,9 +25,14 @@ const isRunning = ref(false)
 
 const isStartButtonDisabled = props.timelineItem.hour !== currentHour()
 
+watch(
+  ()=> props.timelineItem.activityId,
+  () =>  updateTimelineItemActivitySeconds(props.timelineItem, seconds.value)
+)
+
 function start() {
   isRunning.value = setInterval(() => {
-    updateTimelineItemActivitySeconds(props.timelineItem, 1)
+    updateTimelineItemActivitySeconds(props.timelineItem, props.timelineItem.activitySeconds + 1)
 
     seconds.value++
   }, MILLISECONDS_IN_SECONDS)
@@ -42,7 +47,7 @@ function stop() {
 function reset() {
   stop()
 
-  updateTimelineItemActivitySeconds(props.timelineItem, -seconds.value)
+  updateTimelineItemActivitySeconds(props.timelineItem, props.timelineItem.activitySeconds - seconds.value)
 
   seconds.value = 0
 }
