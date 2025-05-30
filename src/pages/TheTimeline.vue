7accd83810c9eafbd8dfc01cd24ab813acdfcbeb
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-05-30 15:49:08 +0300

    Улучшение компонента пункта навигации

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,27 +1,28 @@
 <script setup>
-import { onActivated } from 'vue'
+import { onActivated, onDeactivated } from 'vue'
 import TimelineItem from '../components/TimelineItem.vue'
-import {
-  timelineItems,
-  timelineItemRefs,
-  scrollToCurrentHour,
-} from '@/timeline-items.js'
+import { timelineItems, timelineItemRefs, scrollToCurrentHour } from '@/timeline-items.js'
 
 import TheTimelineIndicator from '@/components/TheTimelineIndicator.vue'
+import { startTimer, stopTimer } from '@/time'
 
-onActivated(scrollToCurrentHour)
+onActivated(() => {
+  scrollToCurrentHour()
+  startTimer()
+})
+onDeactivated(stopTimer)
 </script>
 
 <template>
-<div class="relative">
-  <TheTimelineIndicator />
-  <ul class="mt-7">
-    <TimelineItem
-      v-for="timelineItem in timelineItems"
-      :key="timelineItem.hour"
-      :timeline-item="timelineItem"
-      ref="timelineItemRefs"
-    />
-  </ul>
-</div>
+  <div class="relative">
+    <TheTimelineIndicator />
+    <ul class="mt-7">
+      <TimelineItem
+        v-for="timelineItem in timelineItems"
+        :key="timelineItem.hour"
+        :timeline-item="timelineItem"
+        ref="timelineItemRefs"
+      />
+    </ul>
+  </div>
 </template>
