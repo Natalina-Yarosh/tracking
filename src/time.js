ed916fb9b6bc826092c041891c058f90df6e0f57
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-05-30 15:17:35 +0300

    Реактивный объект даты

--- src/time.js
+++ src/time.js
@@ -1,39 +1,23 @@
-import { computed, ref, watchEffect } from 'vue'
-import {
-  HUNDRED_PERCENT,
-  MILLISECONDS_IN_SECONDS,
-  MINUTES_IN_HOUR,
-  SECONDS_IN_DAY,
-  SECONDS_IN_MINUTE,
-} from '@/constants.js'
+import { computed, ref } from 'vue'
+import { HUNDRED_PERCENT, MILLISECONDS_IN_SECONDS, SECONDS_IN_DAY } from '@/constants.js'
 
-function calculateSecondsSinceMidnight() {
-  const now = new Date()
-
-  now.setHours(23)
-
-  return (
-    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-    SECONDS_IN_MINUTE * now.getMinutes() +
-    now.getSeconds()
-  )
-}
-
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
+export const now = ref(new Date())
 
 export const secondsSinceMidnightInPercentage = computed(
   () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY,
 )
-watchEffect(() => {
-  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-    secondsSinceMidnight.value = 0
-  }
-})
+
+const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
+const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECONDS)
+
 let timer = null
 
 export function startTimer() {
-  secondsSinceMidnight.value = calculateSecondsSinceMidnight()
-  timer = setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECONDS)
+  now.value = new Date()
+  timer = setInterval(
+    () => (now.value = new Date(now.value.getTime() + 5 * 60 * MILLISECONDS_IN_SECONDS)),
+    MILLISECONDS_IN_SECONDS,
+  )
 }
 
 export function stopTimer() {
