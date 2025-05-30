84e13565b3ca2c864b963ff081c6cbb2b533d4f4
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-05-30 16:19:58 +0300

    Загрузка состояния приложения из локального хранилища

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import './assets/main.css'
-
 import * as storage from '@/storage.js'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import { activities } from './activities'
 import { timelineItems } from './timeline-items'
 
+loadState()
+
 document.addEventListener('visibilitychange', () => {
-  if (document.visibilityState === 'hidden') {
-    console.log('hidden')
-    saveState()
-  }
+  document.visibilityState === 'hidden' ? saveState() : loadState()
 })
 
 function saveState() {
@@ -21,4 +18,9 @@ function saveState() {
   })
 }
 
+function loadState() {
+  const state = storage.load()
+  timelineItems.value = state.timelineItems
+  activities.value = state.activities
+}
 createApp(App).mount('#app')
