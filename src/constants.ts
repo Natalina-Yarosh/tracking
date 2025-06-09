3412da2e0f2175112efdc8ef836229c4923acdc3
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-06-09 21:54:11 +0300

    Конвертация модуля констант

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,20 @@
 import { generatePeriodSelectOptions } from './functions'
 import { ICON_CLOCK, ICON_LIST_BULLET, ICON_CHART_BAR } from './icons'
 
+type PageName = typeof PAGE_TIMELINE | typeof PAGE_ACTIVITIES | typeof PAGE_PROGRESS
+
 export const LOCAL_STORAGE_KEY = 'tyme-tracker'
 
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 
-export const NAV_ITEMS = [
+interface NavItem {
+  page: PageName
+  icon: any
+}
+
+export const NAV_ITEMS: NavItem[] = [
   {
     page: PAGE_TIMELINE,
     icon: ICON_CLOCK,
@@ -30,12 +37,20 @@ export const MIDNIGHT_HOUR = 0
 export const SECONDS_IN_DAY = HOURS_IN_DAY * SECONDS_IN_HOUR
 export const MILLISECONDS_IN_SECONDS = 1000
 
+type ButtonType =
+  | typeof BUTTON_TYPE_DANGER
+  | typeof BUTTON_TYPE_NEUTRAL
+  | typeof BUTTON_TYPE_PRIMARY
+  | typeof BUTTON_TYPE_SUCCESS
+  | typeof BUTTON_TYPE_WARNING
+
 export const BUTTON_TYPE_DANGER = 'danger'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_PRIMARY = 'primary'
 export const BUTTON_TYPE_SUCCESS = 'success'
 export const BUTTON_TYPE_WARNING = 'warning'
-export const BUTTON_TYPES = [
+
+export const BUTTON_TYPES: ButtonType[] = [
   BUTTON_TYPE_DANGER,
   BUTTON_TYPE_NEUTRAL,
   BUTTON_TYPE_PRIMARY,
@@ -43,7 +58,7 @@ export const BUTTON_TYPES = [
   BUTTON_TYPE_WARNING,
 ]
 
-export const PERIOD_SELECT_OPTIONS = generatePeriodSelectOptions()
+export const PERIOD_SELECT_OPTIONS: any = generatePeriodSelectOptions()
 
 export const LOW_PERCENT = 33
 export const MEDIUM_PERCENT = 66
